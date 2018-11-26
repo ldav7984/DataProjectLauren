a209d968aa34ded110632e3fef906fc188a3c5f1
Davis, Lauren <ldav7984@740s-pl-109.local>
2018-11-26 12:12:54 -0700

    Initial Commit

---
+++ DataProjectLauren/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DataProjectLauren
+//
+//  Created by Davis, Lauren on 11/26/18.
+//  Copyright © 2018 Davis, Lauren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
