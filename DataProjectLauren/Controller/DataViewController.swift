003694d878dbbcea579e7381969f0f187c58d3f6
Davis <ldav7984@740s-pl-109.local>
2018-11-30 12:50:42 -0700

    Completed given override functions in DataViewController.

--- DataProjectLauren/Controller/DataViewController.swift
+++ DataProjectLauren/Controller/DataViewController.swift
@@ -27,14 +27,24 @@ class DataViewController: UITableViewController
     override func numberOfSections(in tableView: UITableView) -> Int
     {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return bucketList.count
     }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "dataIdentifier", for: indexPath) as! BucketItemCell
+        
+        cell.currentBucketItem = bucketList[indexPath.row]
+        
+        return cell
+    }
+    
     
     lazy var bucketList : [BucketItem] =
     {
@@ -64,17 +74,10 @@ class DataViewController: UITableViewController
         }
         return items
     } //end of loadBucketListFromFile()
-
     
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
+    
+  
+ 
 
     /*
     // Override to support conditional editing of the table view.
