1576589449e5487bc87e6037b3a7c50e2fc8f57c
Davis <ldav7984@740s-pl-109.local>
2018-11-30 11:48:42 -0700

    Created two controller files and added a file to resources.

---
+++ DataProjectLauren/Controller/BucketItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  BucketItemCell.swift
+//  DataProjectLauren
+//
+//  Created by Davis, Lauren on 11/30/18.
+//  Copyright © 2018 Davis, Lauren. All rights reserved.
+//
+
+import UIKit
+
+class BucketItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ DataProjectLauren/Model/BucketItem.swift
@@ -0,0 +1,9 @@
+//
+//  BucketItem.swift
+//  DataProjectLauren
+//
+//  Created by Davis, Lauren on 11/30/18.
+//  Copyright © 2018 Davis, Lauren. All rights reserved.
+//
+
+import Foundation
